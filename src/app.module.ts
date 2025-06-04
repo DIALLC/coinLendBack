2143461ee8800427bba084333e98874553cb4aab
DIALTD <moow.ltd@gmail.com>
2025-06-04 19:23:03 +0700

    change db

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,7 +43,7 @@ import { EtherService } from './service/ether.service';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '12345678',
+      password: 'zR4ZYH2khDwmqfNc@',
       database: 'gameMain',
       entities: [
         User,
